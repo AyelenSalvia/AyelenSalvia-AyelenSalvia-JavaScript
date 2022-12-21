da8bddcae2beeca7fdd149866ce9b514aad89bfa
Ayelen Salvia <ayelensalvia@gmail.com>
2022-12-21 09:37:54 -0300

    Idea Salvia

--- main.js
+++ main.js
@@ -1,45 +1,66 @@
 // USUARIO Y CLAVE
 
-let usuario =prompt("Creá tu usuario");
-let pass = parseInt(prompt("Creá una clave de 6 números "));
-let confirmaUsuario = prompt ("Ingresá tu usuario");
-let confirmaPass = parseInt(prompt("Ingresá tu clave"));
+let divRespuesta = document.querySelector("#respuesta");
 
 
-while(usuario != confirmaUsuario || pass != confirmaPass  ) {
-    alert(" El usuario o la clave es incorecto, volvé a intentarlo");
-    confirmaUsuario = prompt ("Cuál es tu usuario?");
-    confirmaPass = parseInt(prompt("Cuál es tu clave?"));
-    
+function loguear (){
+
+    let usuario =document.getElementById("usuarioNuevo").value;  
+    let pass = parseInt( document.getElementById("contrasenaNueva").value);
+    let confirmaUsuario = document.getElementById("usuario").value;
+    let confirmaPass = parseInt( document.getElementById("contrasena").value);
+
+    if (usuario == confirmaUsuario && pass == confirmaPass) {
+        divRespuesta.innerHTML = "Felicitaciones, ya podes calcular tu Índice de Masa Corporal <a href='./pages/imc.html'>Clickea acá</a>";
+
+
+            
+            }
+    else {
+        alert(" El usuario o la clave es incorecto, volvé a intentarlo")
+    }
 }
 
-if (usuario == confirmaUsuario && pass == confirmaPass) {
-    alert("Felicitaciones, ya podes calcular tu Índice de Masa Corporal ")
-    }
 
-let peso = parseFloat(prompt("Cuál es tu peso en kg?"))
-let altura = parseFloat(prompt("Cuál es tu altura en cm?"))
-let alturaMts= (altura / 100)
-let imc = (peso / Math.pow(alturaMts,2))
-let resultado = imc.toFixed(1)
 
-//ÍNDICE DE MASA CORPORAL
+//CALCULADORA IMC
+const imcForm = document.querySelector("#imcForm")
+
+let divResultadoImc = document.querySelector("#resultadoImc");
+
+function calcularImc () {
+let peso = document.getElementById("peso").value;
+let altura = parseInt( document.getElementById("altura").value);
+let alturaMts= (altura / 100);
+let imc = (peso / Math.pow(alturaMts,2));
+let calculo= "";
+
 
 if (imc <= 18.5){
-    alert(` Peso ${peso} kg, altura ${alturaMts} m, su IMC es igual a ${resultado}= Bajo peso`);
-}else if (resultado > 18.5 && resultado < 25){
-    alert(` Peso ${peso} kg, altura ${alturaMts} m, su IMC es igual a ${resultado}= Peso normal`);
+    calculo = "Bajo peso";
+}else if (imc > 18.5 && imc < 25){
+    calculo = "Peso normal";
 }else if (imc >=25 && imc < 30){
-    alert(` Peso ${peso} kg, altura ${alturaMts} m, su IMC es igual a ${resultado}= Sobrepeso`);
+    calculo ="Sobrepeso";
 }else if (imc >=30 && imc < 35){
-    alert(` Peso ${peso} kg, altura ${alturaMts }m, su IMC es igual a ${resultado }= Obesidad grado 1 `);
+    calculo = "Obesidad grado 1";
 }else if (imc >=35 && imc < 40){
-    alert(` Peso ${peso} kg, altura ${alturaMts} m, su IMC es igual a ${resultado}= Obesidad grado 2`);
+    calculo = "Obesidad grado 2";
 }else if (imc >= 40 ) {
-    alert(` Peso ${peso} kg, altura ${altura} m, su IMC es igual a ${resultado}= Obesidad grado 3`);
+    calculo =  "Obesidad grado 3";
 }
 
+let resultado =  ` Peso ${peso} kg, altura ${alturaMts} m, su IMC es igual a ${imc.toFixed(1)} y su clasificación es ${calculo}`
+divResultadoImc.innerHTML = resultado +" "+  "//<a href='../pages/alumnos.html'>Unite a la comunida Vitality Fitness</a>"
+
+}
+
+
+
+
 //AGREGAR ALUMNOS
+
+const formulario = document.querySelector("#formulario");
 class Alumno {
     constructor(nombreApellido , imc, horasSueno,tiempoActFisica){
         this.nombre= nombreApellido;
@@ -50,7 +71,9 @@ class Alumno {
 }
 
 
-const listaAlumnos =[
+let divAlumnos = document.querySelector("#alumnos");
+
+const listaAlumnos = [
     {nombre: `Ana María`, imc: 30, horasSueno:8, tiempoActFisica: 1},
     {nombre: `Uriel`, imc: 23, horasSueno:7, tiempoActFisica: 3},
     {nombre: `Nahuel`, imc: 21.5, horasSueno:8, tiempoActFisica: 2},
@@ -59,25 +82,52 @@ const listaAlumnos =[
     {nombre: `Liliana`, imc: 22, horasSueno:7, tiempoActFisica: 1},
     {nombre: `Cecilia`, imc: 32, horasSueno:8, tiempoActFisica: 2},
     {nombre: `Jacinto`, imc: 23.5, horasSueno:6, tiempoActFisica: 3},
-    
-    
 ];
 
-const agregarAlumnos=()=> {
-    let nombre= prompt("Ingresa tu Nombre y apellido");
-    let imc = parseFloat(prompt("Cuál es tu IMC actual?"));
-    let horasSueno = parseFloat(prompt("Cuántas horas dormis generalmente?"));
-    let tiempoActFisica=parseFloat(prompt("Cuántas horas de actividad física haces habitualmente por día?"));
-
-    let alumnoNuevo = new Alumno (nombre, imc, horasSueno, tiempoActFisica)
 
 
-listaAlumnos.push (alumnoNuevo);
+
+
+const agregarAlumno=()=> {
+    let nombre= document.getElementById("nombre").value;
+    let imc = parseInt( document.getElementById("imcCalculado").value);
+    let horasSueno = parseInt( document.getElementById("sueno").value);
+    let tiempoActFisica= parseInt( document.getElementById("actividadF").value);
+
+    let alumnoNuevo = new Alumno (nombre, imc, horasSueno, tiempoActFisica);
+
+
+let listaStorage = JSON.parse(localStorage.getItem("alumnos"));
+listaStorage.push(alumnoNuevo);
+localStorage.setItem("alumnos", JSON.stringify (listaStorage));
+
 return listaAlumnos;
+
 }
 
-//FILTRAR ALUMNOS CON SOBREPESO SEGUN IMC
+formulario.onsubmit = (e)=> { 
+    e.preventDefault();
+    agregarAlumno()
+}
 
-console.log(listaAlumnos.filter(alumno => alumno.imc >=30 && imc < 35
-));
+
+
+const mostrarAlumnos = ()=> {
+    const alumnosGuardados = JSON.parse(localStorage.getItem("alumnos"));
+
+    alumnosGuardados.forEach(alumno => {
+        divAlumnos.innerHTML += `
+                        <div class="alumno">
+                            <h4>Nombre del alumno: ${alumno.nombre}</h4>
+                            <h4>IMC:${alumno.imc}</h4>
+                            <h4>Horas de sueño: ${alumno.horasSueno}</h4>
+                            <h4>Horas de actividad fisica por día: ${alumno.tiempoActFisica}</h4>
+                        </div>
+                        `
+    })
+}
+
+
+
+
 
